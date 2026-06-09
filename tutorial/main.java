c0de6fc8f575911ca6f8b69c0fc35a29f9ba6168
M. Fadul <75513829+DevFadul@users.noreply.github.com>
2026-06-09 04:39:12 +0000

    feat: implement Student class and main program

---
+++ tutorial/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Fadul", 22, 3.82);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
